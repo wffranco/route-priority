881a9faa816e6be2b643cc388a459b33a10669f5
langaner <krashgt@gmail.com>
2016-03-15 23:46:33 +0200

    add fix to runController

--- src/Route.php
+++ src/Route.php
@@ -1,6 +1,8 @@
 <?php namespace Langaner\RoutePriority;
 
 use Illuminate\Routing\Route as IlluminateRoute;
+use Illuminate\Http\Request;
+use Illuminate\Routing\ControllerDispatcher;
 
 class Route extends IlluminateRoute
 {
@@ -8,6 +10,23 @@ class Route extends IlluminateRoute
 	 * @var int
 	 */
 	protected $priority = Router::DEFAULT_PRIORITY;
+
+	/**
+     * Run the route action and return the response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+     */
+    protected function runController(Request $request)
+    {
+    	$this->router = app('router');
+        list($class, $method) = explode('@', $this->action['uses']);
+        
+        return (new ControllerDispatcher($this->router, $this->container))
+                    ->dispatch($this, $request, $class, $method);
+    }
 	
 	/**
 	 * @return int
